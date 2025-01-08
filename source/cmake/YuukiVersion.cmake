b40660ad7d7841bb5330a972ac4aac94eb360ae3
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-01-08 22:55:57 +0800

    Update YuukiVersion.cmake

--- source/cmake/YuukiVersion.cmake
+++ source/cmake/YuukiVersion.cmake
@@ -10,7 +10,7 @@ execute_process(COMMAND
     )
 
 if(NOT "${MOD_BUILD}" STREQUAL "Asuna")
-    set(X265_BASE_BRANCH "bitbucket-git")
+    set(X265_BASE_BRANCH "master")
 else()
     set(X265_BASE_BRANCH "old-stable")
 endif()
