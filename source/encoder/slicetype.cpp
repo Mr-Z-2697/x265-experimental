5cca30eb04fbb2e9bc718624f70174accc735baf
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-03-19 01:30:20 +0800

    hdr10-opt: try to get lumaAvg from m_fencPic

--- source/encoder/slicetype.cpp
+++ source/encoder/slicetype.cpp
@@ -610,8 +610,7 @@ void LookaheadTLD::calcAdaptiveQuantFrame(Frame *curFrame, x265_param* param)
 
                         if (param->bHDR10Opt)
                         {
-                            uint32_t sum = lumaSumCu(curFrame, blockX, blockY, param->rc.qgSize);
-                            uint32_t lumaAvg = sum / (loopIncr * loopIncr);
+                            uint32_t lumaAvg = (uint32_t)round(curFrame->m_fencPic->m_avgLumaLevel);
                             if (lumaAvg < 301)
                                 qp_adj += 3;
                             else if (lumaAvg >= 301 && lumaAvg < 367)
