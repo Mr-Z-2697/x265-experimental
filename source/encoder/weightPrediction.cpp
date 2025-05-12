13e3fd70fe5b014af9e0385132698e0b160f4c99
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-05-12 13:45:38 +0800

    wrong var

--- source/encoder/weightPrediction.cpp
+++ source/encoder/weightPrediction.cpp
@@ -467,7 +467,7 @@ void weightAnalyse(Slice& slice, Frame& frame, x265_param& param)
             }
 
             if (!bFound || (minscale == (1 << mindenom) && minoff == 0) || (float)minscore / origscore > 0.998f ||
-                (plane && ((128*minscale) >> mindenom) > (512 - 1 - minscale)) )
+                (plane && ((128*minscale) >> mindenom) > (512 - 1 - minoff)) )
             {
                 SET_WEIGHT(weights[plane], false, 1 << denom, denom, 0);
             }
