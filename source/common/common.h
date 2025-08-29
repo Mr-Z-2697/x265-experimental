35962bbe9d80cd12e0a3092a69773a8cca28f392
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-08-30 01:25:32 +0800

    reduce NUM_CHROMA_MODE to 1
    in combination with previous commits, sets chroma intra pred mode to the same as luma which is all DC now

--- source/common/common.h
+++ source/common/common.h
@@ -317,7 +317,7 @@ typedef int16_t  coeff_t;      // transform coefficient
 #define VER_IDX                     26 // index for intra VERTICAL   mode
 #define HOR_IDX                     10 // index for intra HORIZONTAL mode
 #define DC_IDX                      1  // index for intra DC mode
-#define NUM_CHROMA_MODE             5  // total number of chroma modes
+#define NUM_CHROMA_MODE             1  // total number of chroma modes
 #define DM_CHROMA_IDX               36 // chroma mode index for derived from luma intra mode
 
 #define MDCS_ANGLE_LIMIT            4 // distance from true angle that horiz or vertical scan is allowed
